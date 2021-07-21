94d23562db9401e6fa8f7ffb6d8fe70ed3660208
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2021-07-21 17:45:38 -0500

    Addid components, such as Examplepage, formsign, signin and signup

---
+++ angularexample/src/app/components/examplepage/examplepage.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExamplepageComponent } from './examplepage.component';
+
+describe('ExamplepageComponent', () => {
+  let component: ExamplepageComponent;
+  let fixture: ComponentFixture<ExamplepageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ExamplepageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExamplepageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angularexample/src/app/components/formsign/formsign.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formsign',
+  templateUrl: './formsign.component.html',
+  styleUrls: ['./formsign.component.css']
+})
+export class FormsignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
