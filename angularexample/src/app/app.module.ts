a5299f4ae0ac31e3c46b23dbe632d2ba5bdd0966
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2021-07-21 17:43:55 -0500

    Adding the path to the angular app

--- angularexample/src/app/app.module.ts
+++ angularexample/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { FormsignComponent } from './components/formsign/formsign.component';
+import { ExamplepageComponent } from './components/examplepage/examplepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    FormsignComponent,
+    ExamplepageComponent
   ],
   imports: [
     BrowserModule,
