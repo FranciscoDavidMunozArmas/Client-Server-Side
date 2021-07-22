34a3e7744f53d0fff9dc61ac4636a1f43a3bc8bd
Jorge <jlgalarza3@espe.edu.ec>
2021-07-22 17:40:34 -0500

    Service Routes: Get pull and delete by ID

--- appschedulingServer/src/app/routes/service.routes.ts
+++ appschedulingServer/src/app/routes/service.routes.ts
@@ -8,3 +8,10 @@ router.route("/")
 .get(Controller.getAll)
 .post(Controller.post)
 .delete(Controller.deleteAll);
+
+router.route("/:serviceID")
+.get(Controller.getByID)
+.put(Controller.put)
+.delete(Controller.deleteByID);
+
+export default router;
