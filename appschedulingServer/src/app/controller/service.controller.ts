53a6223235d68bcf41a622557b95490654714180
Jorge <jlgalarza3@espe.edu.ec>
2021-07-22 17:44:13 -0500

    Service controller: Post Service, insert into.

--- appschedulingServer/src/app/controller/service.controller.ts
+++ appschedulingServer/src/app/controller/service.controller.ts
@@ -15,3 +15,17 @@ export const getAll = async (req: Request, res: Response) => {
         });
     }
 }
+
+export const post = async (req: Request, res: Response) => {
+    try {
+        const service: Service = req.body;
+        const conn = await connect();
+        await conn.query(`insert into service set ?`, [service]);
+        return res.status(200).json(service);
+    } catch (error: any) {
+        return res.status(500).json({
+            message: "Error",
+            error: error.message
+        });
+    }
+}
