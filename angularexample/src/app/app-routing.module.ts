0d97cb001b903fd2c547c9c9c36abed9b5dcc9e6
Jorge <jlgalarza3@espe.edu.ec>
2021-08-03 13:56:45 -0500

    Login and Register with Angular/Material added

--- angularexample/src/app/app-routing.module.ts
+++ angularexample/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 const routes: Routes = [
+  {path: '', component:RegisterUserComponent}
 ];
 
 @NgModule({
--- angularexample/src/app/app.module.ts
+++ angularexample/src/app/app.module.ts
@@ -7,17 +7,39 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { CalendarcontainerComponent } from './components/calendarcontainer/calendarcontainer.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
-    CalendarcontainerComponent
+    CalendarcontainerComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatTableModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule, 
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
