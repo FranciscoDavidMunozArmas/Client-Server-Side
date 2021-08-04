a85b2a6ae004d93e47d99862c2258e5cf6cd8139
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2021-08-04 17:57:07 -0500

    Changes on the modal User information, applying of the close event

--- angularexample/src/app/components/userinfo/userinfo.component.ts
+++ angularexample/src/app/components/userinfo/userinfo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-userinfo',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserinfoComponent implements OnInit {
 
+  @Output() closeEvent = new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  closeInfo(){
+    this.closeEvent?.emit();
   }
 
 }
