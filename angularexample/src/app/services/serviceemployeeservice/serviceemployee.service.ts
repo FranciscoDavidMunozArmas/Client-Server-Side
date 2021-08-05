7de1fb6fcb2b0a7222ffa7213f8d34bf182ea86e
Jorge <jlgalarza3@espe.edu.ec>
2021-08-05 16:26:05 -0500

    Correcting import error

--- angularexample/src/app/services/serviceemployeeservice/serviceemployee.service.ts
+++ angularexample/src/app/services/serviceemployeeservice/serviceemployee.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Service } from '../service';
 import { HttpClient } from '@angular/common/http';
 
-import { ServiceEmployee } from '../';
 
 @Injectable({
   providedIn: 'root'
