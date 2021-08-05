54b2cea3cd22fc6d90ef125484be1a3254f71e08
Jorge <jlgalarza3@espe.edu.ec>
2021-08-05 15:34:47 -0500

    Deleting getUserfromAgenda from useragenda.controller

--- appschedulingServer/src/app/controller/useragenda.controller.ts
+++ appschedulingServer/src/app/controller/useragenda.controller.ts
@@ -1,17 +1,3 @@
 import { Request, Response } from 'express';
 
 import { connect } from '../../config/database.config';
-
-export const getUserByAgenda = async (req: Request, res: Response) => {
-    try {
-        const { agendaID } = req.params;
-        const conn = await connect();
-        const users = await conn.query("select * from agenda, user where user.usercode=agenda.usercode and agenda.agendacode=?", [agendaID]);
-        return res.status(200).json(users[0]);
-    } catch (error: any) {
-        return res.status(500).json({
-            message: "Error",
-            error: error.message
-        });
-    }
-}
