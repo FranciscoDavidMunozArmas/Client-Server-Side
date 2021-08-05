ab930308786942105f45410df45877a847578dfc
elianjl <ejllorente@espe.edu.ec>
2021-08-05 15:56:34 -0500

    Appointment service

--- angularexample/src/app/services/appointmentserviceservice/appointmentservice.service.ts
+++ angularexample/src/app/services/appointmentserviceservice/appointmentservice.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppointmentService } from '../appointmentservice/appointment.service';
+import { Service } from '../service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppointmentserviceService {
 
-  constructor() { }
+export class AppointmentserviceService extends Service{
+
+  constructor(private httpClient: HttpClient) {
+    super();
+    super.extendsURI("/appointmentservice");
+   }
 }
