b31b0e90ab72f3e691da09d04eb24f2a7a0a52d3
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2021-08-05 17:37:16 -0500

    Fixing Elian's bugs

--- angularexample/src/app/services/appointmentserviceservice/appointmentservice.service.ts
+++ angularexample/src/app/services/appointmentserviceservice/appointmentservice.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Service } from '../service';
 
 @Injectable({
   providedIn: 'root'
 })
+export class AppointmentserviceService {
 
+  constructor() { }
+}
