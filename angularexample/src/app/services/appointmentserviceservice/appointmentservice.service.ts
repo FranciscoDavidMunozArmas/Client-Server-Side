fa17463f39658db6f94d2371871fa7c324055c5c
elianjl <ejllorente@espe.edu.ec>
2021-08-05 16:37:07 -0500

    Delete Appointment Service

--- angularexample/src/app/services/appointmentserviceservice/appointmentservice.service.ts
+++ angularexample/src/app/services/appointmentserviceservice/appointmentservice.service.ts
@@ -1,45 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AppointmentService } from '../appointmentservice/appointment.service';
 import { Service } from '../service';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class AppointmentserviceService extends Service{
-
-  constructor(private httpClient: HttpClient) {
-    super();
-    super.extendsURI("/appointmentservice");
-  }
-  getAll(){
-    return this.httpClient.get(super.getURI());
-  }
-
-  getByAppointmentId(id:string){
-    return this.httpClient.get(`${super.getURI()}/${id}`);
-  }
-
-  postAppointment(appointmentservice:AppointmentService){
-    return this.httpClient.post<AppointmentService>(super.getURI(), appointmentservice);
-  }
-
-  getByServiceId(id:string){
-    return this.httpClient.get(`${super.getURI()}/${id}`);
-  }
-
-  putAppointment(id:string, appointmentservice: AppointmentService){
-    return this.httpClient.put(`${super.getURI()}/${id}`, appointmentservice);
-  }
-
-  deleteAll(){
-    return this.httpClient.delete(super.getURI());
-  }
-
-  deleteByAppointmentId(id:string){
-    return this.httpClient.delete(`${super.getURI()}/${id}`);
-  }
-
-
-}
--- angularexample/src/app/services/employeeservice/employee.service.ts
+++ angularexample/src/app/services/employeeservice/employee.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Service } from '../service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EmployeeService {
 
-  constructor() { }
-}
