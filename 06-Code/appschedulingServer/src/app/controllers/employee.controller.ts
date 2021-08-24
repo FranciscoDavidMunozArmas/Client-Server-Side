2f618b38551734a073603429b7a75f85c8bf06a2
soleiva03 <s_leivita31@hotmail.com>
2021-08-24 17:45:18 -0500

    Declaration service put

---
+++ 06-Code/appschedulingServer/src/app/controllers/employee.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+import { Employee } from "../interfaces/employee";
+import userSchema from "../schemas/user.schema";
+
+export const putEmployee = async (req: Request, res: Response) =>{}
---
+++ 06-Code/appschedulingServer/src/app/controllers/service.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+import { Service } from "../interfaces/service";
+import userSchema from "../schemas/user.schema";
+
+export const putService = async (req: Request, res: Response) =>{}
