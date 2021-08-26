3c66f5e64bf92a98c2a1159102243639e6bd7454
Jorge <jlgalarza3@espe.edu.ec>
2021-08-26 15:06:40 -0500

    POST Method on Service Controller: Post Service

--- 06-Code/appschedulingServer/src/app/controllers/service.controller.ts
+++ 06-Code/appschedulingServer/src/app/controllers/service.controller.ts
@@ -3,3 +3,19 @@ import { Service } from "../interfaces/service";
 import userSchema from "../schemas/user.schema";
 
 export const putService = async (req: Request, res: Response) =>{}
+
+
+export const postService = async (req: Request, res: Response) => {
+    try {
+        const { userService } = req.params;
+        const service: Service = req.body;
+        const updateService = await userSchema.findByIdAndUpdate(userService, {
+            $push: {
+                services: [service]
+            }
+        }, { new: true });
+        return res.status(200).json(updateService);
+    } catch (error: any) {
+        return res.status(500).json({ message: "error", error: error });
+    }
+}
