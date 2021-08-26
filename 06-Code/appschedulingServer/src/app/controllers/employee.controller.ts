a92b3ba15f51322358e7c1cbb5511bc083060dda
soleiva03 <s_leivita31@hotmail.com>
2021-08-25 19:04:04 -0500

    Service update V1.0

--- 06-Code/appschedulingServer/src/app/controllers/employee.controller.ts
+++ 06-Code/appschedulingServer/src/app/controllers/employee.controller.ts
@@ -2,4 +2,16 @@ import { Request, Response } from "express";
 import { Employee } from "../interfaces/employee";
 import userSchema from "../schemas/user.schema";
 
-export const putEmployee = async (req: Request, res: Response) =>{}
+export const putEmployee = async (req: Request, res: Response) => {
+    try{
+        const {userName} = req.params;
+        const employee: Employee  = req.body;
+        const response = await userSchema.findByIdAndUpdate(userName, employee, {new: true});
+        return res.status(200).json(response);
+    }catch(error: any){
+        return res.status(500).json({
+            message: "Error",
+            error: error.message
+        });
+    }
+}
