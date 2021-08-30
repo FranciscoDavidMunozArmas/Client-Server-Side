ce7f4f021ae3341bbb53997d23753d874b63b711
soleiva03 <s_leivita31@hotmail.com>
2021-08-30 18:03:35 -0500

    Router service V 1.0

--- 06-Code/appschedulingServer/src/app/routes/service.routes.ts
+++ 06-Code/appschedulingServer/src/app/routes/service.routes.ts
@@ -1,4 +1,12 @@
+import {Router} from 'express';
+import * as serviceCotroller from '../controllers/service.controller'
+
+const router = Router();
+
+router.route("/service/:idService")
+    .put(serviceCotroller.putService);
+
+export default router;
 //get
 //post
-//put
 //delete
