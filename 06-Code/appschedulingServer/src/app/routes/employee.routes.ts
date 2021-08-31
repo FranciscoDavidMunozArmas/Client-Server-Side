f6e2783177a38ce267f01801773e62eb71fa5970
soleiva03 <s_leivita31@hotmail.com>
2021-08-31 18:49:39 -0500

    Route pu of employee

--- 06-Code/appschedulingServer/src/app/routes/employee.routes.ts
+++ 06-Code/appschedulingServer/src/app/routes/employee.routes.ts
@@ -2,6 +2,10 @@ import {Router} from 'express';
 import * as employeeCotroller from '../controllers/employee.controller'
 const router = Router();
 
+router.route("/employee/:id")
+    .put(employeeCotroller.putEmployee);
+
+
 //get
 //post
 //put
